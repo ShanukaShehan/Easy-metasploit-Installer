7d719dc05651ed2f6d5c78521f3bc58c1587f563
Shanuka Shehan <57612556+ShanukaOfficial@users.noreply.github.com>
2019-11-11 19:49:54 +0530

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,1 +1,32 @@
+#!/bin/sh
+pkg install figlet
+figlet METASPLOIT
+apt update
+apt upgrade
+pkg install php
+pkg install curl
+pkg install wget
+figlet Metasploit
+figlet Installation
+apt install unstable-repo
+apt install metasploit
+figlet METASPLOIT
+figlet INSTALLED
+clear
+apt update
+apt upgrade
+pkg install figlet
+pkg install php
+pkg install curl
+pkg install wget
+pkg install termux-api
+clear
+figlet METASPLOIT
+figlet INSTALLATION
+apt-get install unstable-repo
+apt-get install metasploit
+figlet METASPLOIT
+figlet INSTALLED
+echo "$g You are Successfuly Installed Metasploit. Enjoy Now!!! $g"
+exit
 
